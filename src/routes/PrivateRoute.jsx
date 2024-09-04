df192f057383860e5f207019790dbd4e4f106235
Oks <witchox@gmail.com>
2024-09-04 15:24:28 +0200

    Refresh-logedin-fixed

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,11 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
 
 export function PrivateRoute({ component }) {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  return isLoggedIn ? component : <Navigate to="login" />;
+  const location = useLocation();
+
+  return isLoggedIn ? component : <Navigate to="/login" state={location} />;
 }
--- src/routes/RestrictedRoute.jsx
+++ src/routes/RestrictedRoute.jsx
@@ -1,9 +1,15 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
 
 export function RestrictedRoute({ component }) {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  return isLoggedIn ? <Navigate to="/" /> : component;
+  const location = useLocation();
+
+  return isLoggedIn ? (
+    <Navigate to={location?.state || "/"} replace />
+  ) : (
+    component
+  );
 }
